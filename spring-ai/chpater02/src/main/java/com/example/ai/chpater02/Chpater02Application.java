0fbab72afd4cb9cdef458c40a66b843531bd174f
iamwotjr99 <iamwotjr@gmail.com>
2026-07-29 16:26:41 +0900

    Spring AI 도서 Chapter02 서버 초기 설정

---
+++ spring-ai/chpater02/src/main/java/com/example/ai/chpater02/Chpater02Application.java
@@ -0,0 +1,13 @@
+package com.example.ai.chpater02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chpater02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chpater02Application.class, args);
+	}
+
+}
