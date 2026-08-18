46a944e9e24c1a70f6b97aed7fb2ed71d4255c44
iamwotjr99 <iamwotjr@gmail.com>
2026-08-18 16:52:40 +0900

    대화 문맥 영속적으로 관리

--- spring-ai/chpater02/src/main/java/com/example/ai/practice/config/AiConfig.java
+++ spring-ai/chpater02/src/main/java/com/example/ai/practice/config/AiConfig.java
@@ -4,6 +4,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +23,14 @@ public class AiConfig {
                         new SimpleLoggerAdvisor()
                 )
                 .defaultSystem("You are a helpful health trainer.")
+                .build();
+    }
+
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(repository)
+                .maxMessages(10)
                 .build();
     }
 }
